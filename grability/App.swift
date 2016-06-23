c71414a318ec45cfbe6e4ba1ea68c29314a48aa8
German Gomez <ggomez1728@gmail.com>
2016-06-23 16:24:07 -0400

    first commit

---
+++ grability/App.swift
@@ -0,0 +1,13 @@
+//
+//  App.swift
+//  grability
+//
+//  Created by Pollinion User on 23/06/16.
+//  Copyright © 2016 Pollinion INC. All rights reserved.
+//
+
+import UIKit
+
+class App: NSObject {
+
+}
---
+++ grability/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  grability
+//
+//  Created by Pollinion User on 23/06/16.
+//  Copyright © 2016 Pollinion INC. All rights reserved.
+//
+
+import UIKit
+
+class Category: NSObject {
+
+}
